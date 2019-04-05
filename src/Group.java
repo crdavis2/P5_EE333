dc2519249e882f20181c4527d11398ca803b6938
Collin Davis <collindavisr@gmail.com>
2019-04-05 11:39:57 -0500

    Cleanup

--- src/Group.java
+++ src/Group.java
@@ -2,23 +2,23 @@
  * File: Group.java
  * Author: Collin Davis crdavis2@uab.edu
  * Assignment:  P5 - EE333 Spring 2019
- * Vers: 1.0.0 04/04/2019 CRD - initial coding
+ * Vers: 1.0.0 04/05/2019 CRD - initial coding
  *
  */
 
 
 /**
- * Class to define Group behavior for the smart light system
+ *
  * @author Collin Davis crdavis2@uab.edu
  */
 public class Group {
     
     private String groupName;
-    
+
     /**
      * Constructor for objects of class Group with name
      * 
-     * @param name text for group name. If null, the group name will
+     * @param name text for name. If null, the group name will
      * be set to "New group"
      *
      */
@@ -29,7 +29,4 @@ public class Group {
             groupName = name;
         }
     }
-    
-    
-
 }
